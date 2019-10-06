a5de39ec0d76fb9c5b0280f097c9ac527768d19d
masha90la <maria.alisievich@gmail.com>
2019-10-06 10:44:42 -0400

    Add proptypes

--- src/components/App.js
+++ src/components/App.js
@@ -64,9 +64,7 @@ class App extends Component {
   render() {
     return (
       <div className="scoreboard">
-        <Header 
-          title="Scoreboard" 
-          players={this.state.players}
+        <Header players={this.state.players}
         />
   
         {/* Players list */}
